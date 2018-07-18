314f724bf2b42fd0ab36bfc4297beda17049b197
Dina Hafez <dinahafez.8@gmail.com>
2018-07-18 13:19:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -18,19 +18,3 @@ require 'pry'
 #   end
 #   new_hash
 # end
-
-def reformat_languages(languages)
-  # your code here
-  new_hash = {}
-  style_array = []
-  languages.each do |styles, data|
-    data.each do |language, type|
-      type.each do |k, v|
-        style_array << styles
-        new_hash[language] = {k => v, :style => style_array} 
-      end
-    end
-  end
-  new_hash
-  binding.pry
-end
